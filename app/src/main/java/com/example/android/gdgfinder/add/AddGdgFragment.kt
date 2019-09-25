f253bc2bd29d4bc5f86392692e67859fa3b35f71
henryfebryan <henry_febryan@yahoo.com>
2019-09-25 16:11:41 +0700

    on button click

--- app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -18,8 +18,10 @@ class AddGdgFragment : Fragment() {
         ViewModelProviders.of(this).get(AddGdgViewModel::class.java)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val binding = AddGdgFragmentBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
@@ -35,6 +37,9 @@ class AddGdgFragment : Fragment() {
                     Snackbar.LENGTH_SHORT // How long to display the message.
                 ).show()
                 viewModel.doneShowingSnackbar()
+
+                binding.button.contentDescription = getString(R.string.submitted)
+                binding.button.text = getString(R.string.done)
             }
         })
 
